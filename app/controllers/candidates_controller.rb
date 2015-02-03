36841fad6ce3cebc8123ae06537fc816977c503a
Aleksi Knuutila <aleksi@aleksis-air-3.default>
2015-02-03 12:24:37 +0000

    first steps for looking up policies for each candidate

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -3,6 +3,34 @@ class CandidatesController < ApplicationController
   require 'json'
   require 'open-uri'
 
+  POLICY_AREAS = [ 'Environment', 'Welfare', 'Immigration', 'Austerity' ]
+  PARTY_POLICIES = { 
+    'Conservative Party' => { 'Environment' => 'For conservatives, the environment is very important',
+      'Welfare' => 'For conservatives, welfare is very important',
+      'Immigration' => 'For conservatives, immigration is very important',
+      'Austerity' => 'For conservatives, austerity is very important' },
+    'Labour Party' => { 'Environment' => 'For labour, the environment is very important',
+      'Welfare' => 'For labour, welfare is very important',
+      'Immigration' => 'For labour, immigration is very important',
+      'Austerity' => 'For labour, austerity is very important' },
+    'Liberal Democrats' => { 'Environment' => 'For libdems, the environment is very important',
+      'Welfare' => 'For libdems, welfare is very important',
+      'Immigration' => 'For libdems, immigration is very important',
+      'Austerity' => 'For libdems, austerity is very important' },
+    'Green Party' => { 'Environment' => 'For greens, the environment is very important',
+      'Welfare' => 'For greens, welfare is very important',
+      'Immigration' => 'For greens, immigration is very important',
+      'Austerity' => 'For greens, austerity is very important' },
+    'UKIP' => { 'Environment' => 'For ukip, the environment is very important',
+      'Welfare' => 'For ukip, welfare is very important',
+      'Immigration' => 'For ukip, immigration is very important',
+      'Austerity' => 'For ukip, austerity is very important' },
+    'Default' => { 'Environment' => 'No information about Environment',
+      'Welfare' => 'No information about welfare',
+      'Immigration' => 'No information about immigration',
+      'Austerity' => 'No information about austerity' }
+  }
+
   def getConstituencyId(postcode)
     uri = 'http://mapit.mysociety.org/postcode/'+postcode
     jsondata = open(uri)
@@ -10,14 +38,23 @@ class CandidatesController < ApplicationController
     return @data['shortcuts']['WMC']
   end
 
-  def removeDuplicates(c)
+  def getPolicy(candidate, policyArea)
+    party = candidate['party_memberships']['2015']['name']
+    if not PARTY_POLICIES.key?(party)
+      party = 'Default'
+    end
+    return PARTY_POLICIES[party][policyArea]
   end
 
-  def getPolicies(candidate, policyArea)
-#    policies = { 'Environment': {'Conservative Party': 'Cons',
-#                                 'Green Party': 'Greens'} }
-    party = @candidate['party_memberships']['2015']['name']
-    byebug
+  def getAllPolicies(candidates)
+    policies = Hash.new
+    for candidate in candidates
+      policies[candidate['id']] = Hash.new
+      for policy in POLICY_AREAS
+        policies[candidate['id']][policy] = getPolicy(candidate, policy)
+      end
+    end
+    return policies
   end
 
   def getCandidates(constituencyId)
@@ -48,6 +85,7 @@ class CandidatesController < ApplicationController
     @q = request['q']
     @conId = getConstituencyId(@q)
     @candidates = getCandidates(@conId)
+    @policies = getAllPolicies(@candidates)
     # necessary?
     return ''
   end
