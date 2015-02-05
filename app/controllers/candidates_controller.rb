c2df9f8db82d3b7ea1b7813791cf2f4a2875ad81
Aleksi Knuutila <aleksi@aleksis-air-3.default>
2015-02-05 00:12:26 +0000

    improved system for displaying policies and added policy info on immigration

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -8,7 +8,7 @@ class CandidatesController < ApplicationController
     'Conservative Party' => { 
       'NHS' => 'For conservatives, the NHS is very important',
       'UK Economy' => 'For conservatives, the economy is very important',
-      'Immigration' => 'For conservatives, immigration is very important',
+      'Immigration' => [ [ 'Require European jobseekers to leave if they are unemployed after 6 months. Renegotiate the rules for free movement within the EU. Make migrants wait 4 years before claiming certain benefits or social housing.', 'http://press.conservatives.com/post/103802921280/david-cameron-speech-on-immigration' ], [ 'Reduce the incentives for people to move to the UK, with the aim of lowering net immigration to below 100,000 people a year (currently 243,000).', 'https://www.conservatives.com/Plan/CapWelfareReduceImmigration.aspx' ] ],
       'Benefits and pensions' => 'For conservatives, welfare is very important',
       'Europe' => 'For conservatives, europe is very important',
       'Environment' => 'For conservatives, the environment is very important',
@@ -16,7 +16,7 @@ class CandidatesController < ApplicationController
     'Labour Party' => {
       'NHS' => 'For labour, the NHS is very important',
       'UK Economy' => 'For labour, the economy is very important',
-      'Immigration' => 'For labour, immigration is very important',
+      'Immigration' => [ ['Establish stronger border control to reduce illegal immigration, with "proper" entry adn exit checks. Reduce low-skilled migration with "smarter targets", while ensuring university students and high-skilled workers can migrate. Increase fines for employing illegal immigrants, and outlaw employment agencies recruiting only abroad.', 'http://press.labour.org.uk/post/98301589749/speech-by-yvette-cooper-mp-to-labours-annual' ] ],
       'Benefits and pensions' => 'For labour, welfare is very important',
       'Europe' => 'For labour, europe is very important',
       'Environment' => 'For labour, the environment is very important',
@@ -24,7 +24,7 @@ class CandidatesController < ApplicationController
     'Liberal Democrats' => { 
       'NHS' => 'For libdems, the NHS is very important',
       'UK Economy' => 'For libdems, the economy is very important',
-      'Immigration' => 'For libdems, immigration is very important',
+      'Immigration' => [ [ 'Reintroduce exit checks at UK borders, so the government can identify people overstaying their visa. Mandate people on unemployment benefits to take an English test, and require those who fail to attend English classes.', 'http://www.libdems.org.uk/nick_clegg_s_immigration_speech' ] ],
       'Benefits and pensions' => 'For libdems, welfare is very important',
       'Europe' => 'For libdems, europe is very important',
       'Environment' => 'For libdems, the environment is very important',
@@ -32,7 +32,7 @@ class CandidatesController < ApplicationController
     'Green Party' => {
       'NHS' => 'For greens, the NHS is very important',
       'UK Economy' => 'For greens, the economy is very important',
-      'Immigration' => 'For greens, immigration is very important',
+      'Immigration' => [ [ 'Reduce UK immigration controls over time. Migrants illegally in the UK for over five years will be allowed to remain unless they pose a serious danger to public safety.', 'http://policy.greenparty.org.uk/mg.html' ], [ 'Increase legal rights of asylum seekers.', 'http://policy.greenparty.org.uk/ra.html' ] ],
       'Benefits and pensions' => 'For greens, welfare is very important',
       'Europe' => 'For greens, europe is very important',
       'Environment' => 'For greens, the environment is very important',
@@ -40,10 +40,26 @@ class CandidatesController < ApplicationController
     'UK Independence Party (UKIP)' => {
       'NHS' => 'For ukip, the NHS is very important',
       'UK Economy' => 'For ukip, the economy is very important',
-      'Immigration' => 'For ukip, immigration is very important',
+      'Immigration' => [ [ 'Like in Australia, choose immigrants based on a system of points, reflecting skills needed for work in the country. Apply points system to both EU and non-EU immigrants. Reduce net immigration to 50,000 people a year. Make language skill tests for residence permits harder. To allow the UK to return asylum seekers to other EU countries without hearing their case, opt out of the Dublin treaty.', 'http://www.ukip.org/steven_woolfe_ukip_s_ethical_migration_policy' ], [ 'People who currently are in the UK legally would not be deported, were the UK to leave the EU.', '' ] ],
       'Benefits and pensions' => 'For ukip, welfare is very important',
       'Europe' => 'For ukip, europe is very important',
       'Environment' => 'For ukip, the environment is very important',
+    },
+    'Scottish National Party' => {
+      'NHS' => 'For SNP, the NHS is very important',
+      'UK Economy' => 'For SNP, the economy is very important',
+      'Immigration' => [ [ 'Give the devolved Scottish government control over immigration to Scotland. Use a Canadian-style "earned citizenship system" to attract high-skilled workers.', '' ] ],
+      'Benefits and pensions' => 'For SNP, welfare is very important',
+      'Europe' => 'For SNP, europe is very important',
+      'Environment' => 'For SNP, the environment is very important',
+    },
+    'Plaid Cymru' => {
+      'NHS' => 'For plaid cymru, the NHS is very important',
+      'UK Economy' => 'For plaid cymru, the economy is very important',
+      'Immigration' => [ [ 'Opposes a choice of immigrants following a "point-based system", as it would not reflect Welsh needs. Asylum seekers should be able to work in Wales while waiting for their status decision. UK government should close "detention centres".', 'http://www.partyof.wales/our-vision-for-a-better-society/?force=1' ] ],
+      'Benefits and pensions' => 'For plaid cymru, welfare is very important',
+      'Europe' => 'For plaid cymru, europe is very important',
+      'Environment' => 'For plaid cymru, the environment is very important',
     },
     'Default' => {
       'NHS' => 'No information about NHS yet',
@@ -64,6 +80,16 @@ class CandidatesController < ApplicationController
     jsondata = open(uri)
     @data = JSON.load(jsondata)
     return @data['shortcuts']['WMC']
+  end
+
+  def isIncumbentMP(candidate)
+    # Is the best way to check this to look at the public whip identifier?
+    for ide in candidate['identifiers']
+      if ide['scheme'] == 'uk.org.publicwhip'
+        return true
+      end
+    end
+    return false
   end
 
   def getPolicy(candidate, policyArea)
@@ -110,8 +136,9 @@ class CandidatesController < ApplicationController
     end
     # For some reason there are duplicates in the JSON
     @candidates = @candidates.uniq
-    for ind in @candidates.each_index()
-      @candidates[ind]['image_url'] = getImageUrl(@candidates[ind])
+    for i in @candidates.each_index()
+      @candidates[i]['image_url'] = getImageUrl(@candidates[i])
+      @candidates[i]['isIncumbentMP'] = isIncumbentMP(@candidates[i])
     end
     return @candidates
   end
@@ -132,6 +159,7 @@ class CandidatesController < ApplicationController
     end
     @candidates = getCandidates(@conId)
     @policies = getAllPolicies(@candidates)
+    @PARTY_POLICIES = PARTY_POLICIES
     # necessary?
     return ''
   end
---
+++ app/controllers/policies.rb
@@ -0,0 +1,34 @@
+Immigration
+
+Conservatives:
+Require European jobseekers to leave if they are unemployed after 6 months. Renegotiate the rules for free movement within the EU. Make migrants wait 4 years before claiming certain benefits or social housing.
+http://press.conservatives.com/post/103802921280/david-cameron-speech-on-immigration
+Reduce the incentives for people to move to the UK, with the aim of lowering net immigration to below 100,000 people a year (currently 243,000).
+https://www.conservatives.com/Plan/CapWelfareReduceImmigration.aspx
+
+Labour:
+Establish stronger border control to reduce illegal immigration, with "proper" entry adn exit checks. Reduce low-skilled migration with "smarter targets", while ensuring university students and high-skilled workers can migrate. Increase fines for employing illegal immigrants, and outlaw employment agencies recruiting only abroad.
+http://press.labour.org.uk/post/98301589749/speech-by-yvette-cooper-mp-to-labours-annual
+
+Lib Dems:
+Reintroduce exit checks at UK borders, so the government can identify people overstaying their visa. Mandate people on unemployment benefits to take an English test, and require those who fail to attend English classes.
+http://www.libdems.org.uk/nick_clegg_s_immigration_speech
+
+SNP:
+Give the devolved Scottish government control over immigration to Scotland.
+Use a Canadian-style "earned citizenship system" to attract high-skilled workers.
+
+Plaid Cymru:
+Opposes a choice of immigrants following a "point-based system", as it would not reflect Welsh needs. Asylum seekers should be able to work in Wales while waiting for their status decision. UK government should close "detention centres".
+http://www.partyof.wales/our-vision-for-a-better-society/?force=1
+
+UKIP:
+Like in Australia, choose immigrants based on a system of points, reflecting skills needed for work in the country. Apply points system to both EU and non-EU immigrants. Reduce net immigration to 50,000 people a year. Make language skill tests for residence permits harder. To allow the UK to return asylum seekers to other EU countries without hearing their case, opt out of the Dublin treaty.
+http://www.ukip.org/steven_woolfe_ukip_s_ethical_migration_policy
+People who currently are in the UK legally would not be deported, were the UK to leave the EU.
+
+Greens:
+Reduce UK immigration controls over time. Migrants illegally in the UK for over five years will be allowed to remain unless they pose a serious danger to public safety.
+http://policy.greenparty.org.uk/mg.html
+Increase legal rights of asylum seekers.
+http://policy.greenparty.org.uk/ra.html
