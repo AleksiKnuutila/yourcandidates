8148799fbd6f82b8f888c8622033eb09d082bbf3
Aleksi Knuutila <aleksi@aleksis-air-3.lan>
2015-02-15 15:01:59 +0000

    changed bg image and text on frontpage

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,8 @@ Rails.application.routes.draw do
   get 'contact/home'
 
   get 'candidates/index'
-  get 'candidates/show'
-
-  get '/candidates/:id', to: 'candidates#show'
+#  get 'candidates/show'
+#  get '/candidates/:id', to: 'candidates#show'
 
   get 'welcome/index'
   root 'welcome#index'
